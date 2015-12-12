03e1c6ca3391d83d06b701669185004504880536
Brian <brian@ubuntu>
2015-12-12 18:27:26 -0500

    Arg feature update

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -3,7 +3,15 @@ public class Program
 { 
 	public static void Main(string[] args)
 	{ 
-		Console.WriteLine("Hello World from beautiful Core CLR!"); 
+		Console.WriteLine("Hello World from beautiful Core CLR!");
+		if (args.Length > 0)
+		{
+			Console.WriteLine("You entered the following {0} command line arguments:", args.Length );
+			for (int i=0; i < args.Length; i++)
+			{
+				Console.WriteLine("{0}", args[i]);
+			}
+		}
 	} 
 }
 
