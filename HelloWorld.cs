c2a0aec13fa561c05635a519a0839d9dd79cd58c
Brian <brian@ubuntu>
2015-12-12 18:29:30 -0500

    Minor edit

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,7 +6,7 @@ public class Program
 		Console.WriteLine("Hello World from beautiful Core CLR!");
 		if (args.Length > 0)
 		{
-			Console.WriteLine("You entered the following {0} command line arguments:", args.Length );
+			Console.WriteLine("You entered the following {0} command line argument(s):", args.Length );
 			for (int i=0; i < args.Length; i++)
 			{
 				Console.WriteLine("{0}", args[i]);
